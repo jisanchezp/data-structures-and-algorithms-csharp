b79020f9d758b87050d4910bc398fe992e691962
Juan I. Sanchez <juanisanchez@outlook.com>
2026-06-05 23:56:50 -0300

    Three Sum with brute force

--- DataStructuresAndAlgorithms/Program.cs
+++ DataStructuresAndAlgorithms/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿using System.Diagnostics;
+using ADSLib;
+using DataStructuresAndAlgorithms.Services;
+
+var ints = In.ReadInts("Inputs/1Kints.txt").ToArray();
+var watch = new Stopwatch();
+watch.Start();
+var triples = ThreeSum.Count(ints);
+
+Console.WriteLine($"Triples are {triples}");
+Console.WriteLine($"Elapsed time {watch.Elapsed:g}");
+watch.Stop();
